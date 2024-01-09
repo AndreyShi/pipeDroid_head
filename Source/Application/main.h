5a2fdb70db3edfac03514cb04d7a6032b8eea546
a.shiliaev <andrey.shilyayev.89@mail.ru>
2024-01-09 15:27:25 +0300

    set valid ip and dest ip

--- Source/Application/main.h
+++ Source/Application/main.h
@@ -49,9 +49,9 @@
 #endif
 
 /*Static IP ADDRESS: IP_ADDR0.IP_ADDR1.IP_ADDR2.IP_ADDR3 */
-#define IP_ADDR0   192
-#define IP_ADDR1   168
-#define IP_ADDR2   1
+#define IP_ADDR0   169
+#define IP_ADDR1   254
+#define IP_ADDR2   219
 
 /* MAC ADDRESS: MAC_ADDR0:MAC_ADDR1:MAC_ADDR2:MAC_ADDR3:MAC_ADDR4:MAC_ADDR5 */
 #define MAC_ADDR0   0x00
@@ -70,10 +70,10 @@
 #define MAC_ADDR5   0x11
 #endif
 
-#define DEST_IP_ADDR0   192
-#define DEST_IP_ADDR1   168
-#define DEST_IP_ADDR2   1
-#define DEST_IP_ADDR3   10
+#define DEST_IP_ADDR0   169
+#define DEST_IP_ADDR1   254
+#define DEST_IP_ADDR2   219
+#define DEST_IP_ADDR3   106
 
 #define UDP_LOCAL_PORT    	10000   /* define the UDP local connection port */
 #define UDP_SERVER_PORT    	10001   /* define the UDP local connection port */
@@ -85,10 +85,10 @@
 #define NETMASK_ADDR3   0
 
 /*Gateway Address*/
-#define GW_ADDR0   192
-#define GW_ADDR1   168
-#define GW_ADDR2   1
-#define GW_ADDR3   1  
+#define GW_ADDR0   169
+#define GW_ADDR1   254
+#define GW_ADDR2   219
+#define GW_ADDR3   100  
 
 //#define MII_MODE
 
